f6609f32b6c4a5e2dfe17168ee1cd1d24d659bc9
amherstia <asdoctor@up.edu.ph>
2025-05-11 07:29:13 +0800

    chore: redesign user home

--- lib/view/user/home_screen.dart
+++ lib/view/user/home_screen.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:sp_code/model/difficulty.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:sp_code/config/theme.dart';
 import 'package:sp_code/model/user_entity.dart';
-import 'package:sp_code/view/user/difficulty_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   final UserEntity loggedInUser;
@@ -15,39 +14,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Difficulty> _allDifficulties = [];
-  List<Difficulty> _filteredDifficulties = [];
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _fetchDifficulties();
-  }
-
-  Future<void> _fetchDifficulties() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final snapshot =
-        await FirebaseFirestore.instance
-            .collection('difficulties')
-            .orderBy('createdAt', descending: true)
-            .get();
-
-    setState(() {
-      isLoading = false;
-      _allDifficulties =
-          snapshot.docs
-              .map((doc) => Difficulty.fromMap(doc.id, doc.data()))
-              .toList();
-
-      _filteredDifficulties = _allDifficulties;
-    });
-  }
-
   String greeting() {
     var hour = DateTime.now().hour;
     if (hour < 12) {
@@ -63,165 +29,206 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.grey1,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 225,
-            pinned: true,
-            floating: true,
-            centerTitle: false,
-            backgroundColor: AppTheme.primary,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: SafeArea(
-                child: Column(
-                  children: [
-                    SizedBox(height: kToolbarHeight),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Good ${greeting()}, ${widget.loggedInUser.firstName}",
-                            style: TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                              color: AppTheme.white,
-                            ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance
+                .collection("Users")
+                .where('email', isEqualTo: widget.loggedInUser.email)
+                .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final fetchedDocs = snapshot.data!.docs;
+          final currentUser = fetchedDocs[0].data();
+          if (currentUser.isEmpty) {
+            return Center(child: Text("No user found."));
+          }
+
+          return CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 225,
+                pinned: true,
+                floating: true,
+                centerTitle: false,
+                backgroundColor: AppTheme.primary,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: SafeArea(
+                    child: Column(
+                      children: [
+                        SizedBox(height: kToolbarHeight),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Good ${greeting()}, ${widget.loggedInUser.firstName}",
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppTheme.white,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                "Let's test your knowledge today",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppTheme.white.withOpacity(0.8),
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 8),
-                          Text(
-                            "Let's test your knowledge today",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: AppTheme.white.withOpacity(0.8),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.all(16),
-            sliver:
-                isLoading == true
-                    ? SliverToBoxAdapter(
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            AppTheme.primary,
-                          ),
-                          strokeWidth: 2,
-                        ),
-                      ),
-                    )
-                    : _filteredDifficulties.isEmpty && isLoading == false
-                    ? SliverToBoxAdapter(
-                      child: Center(
-                        child: Text(
-                          "No difficulties found",
-                          style: TextStyle(color: AppTheme.text2),
-                        ),
-                      ),
-                    )
-                    : SliverGrid(
-                      delegate: SliverChildBuilderDelegate(
-                        childCount: 3,
-                        (context, index) => _buildDifficultyCard(
-                          _filteredDifficulties[index],
-                          index,
-                        ),
-                      ),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 16,
-                        crossAxisSpacing: 16,
-                        childAspectRatio: 0.8,
+              SliverPadding(
+                padding: const EdgeInsets.only(left: 16, top: 20),
+                sliver: SliverToBoxAdapter(
+                  child: Text(
+                    "Recent Quizzes",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              currentUser['quizzesTaken'].isNotEmpty
+                  ? SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: currentUser['quizzesTaken'].length,
+                        itemBuilder: (context, index) {
+                          final quiz = currentUser['quizzesTaken'][index];
+                          return _buildRecentQuizCard(quiz, index);
+                        },
                       ),
                     ),
-          ),
-        ],
+                  )
+                  : SliverPadding(
+                    padding: EdgeInsets.all(16),
+                    sliver: SliverToBoxAdapter(
+                      child: Center(child: Text("No recent quizzes yet!")),
+                    ),
+                  ),
+            ],
+          );
+        },
       ),
     );
   }
 
-  Widget _buildDifficultyCard(Difficulty difficulty, int index) {
-    return Card(
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder:
-                      (context) => DifficultyScreen(
-                        difficulty: difficulty,
-                        loggedInUser: widget.loggedInUser,
-                      ),
-                ),
-              );
-            },
-            child: Container(
-              decoration: BoxDecoration(
+  Widget _buildRecentQuizCard(Map<String, dynamic> recentQuiz, int index) {
+    Color _getScoreColor(double score) {
+      if (score >= 0.9) return AppTheme.green;
+      if (score >= 0.5) return AppTheme.secondaryShade;
+      return AppTheme.red;
+    }
+
+    String _formatDate(DateTime date) {
+      return '${date.day}/${date.month}/${date.year}';
+    }
+
+    final scorePercentage = (recentQuiz['quizScore'] * 100).round();
+    return StreamBuilder(
+      stream:
+          FirebaseFirestore.instance
+              .collection('quizzes')
+              .doc(recentQuiz['quizId'])
+              .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        final fetchedQuiz = snapshot.data as DocumentSnapshot;
+        return Card(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16),
-                border: Border.all(color: AppTheme.primary),
               ),
-              padding: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: AppTheme.primary.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Icon(Icons.quiz, size: 48, color: AppTheme.primary),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(16),
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(color: AppTheme.primary),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    difficulty.name,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: AppTheme.text,
-                    ),
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            fetchedQuiz['title'],
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: AppTheme.text,
+                            ),
+                          ),
+                          Text(
+                            "Last Taken on ${_formatDate(recentQuiz['takenAt'].toDate())}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: AppTheme.text2,
+                            ),
+                          ),
+                        ],
+                      ),
+                      CircularPercentIndicator(
+                        radius: 30,
+                        lineWidth: 10,
+                        animation: true,
+                        animationDuration: 1500,
+                        percent: recentQuiz['quizScore'],
+                        center: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              '${scorePercentage}%',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: _getScoreColor(recentQuiz['quizScore']),
+                              ),
+                            ),
+                          ],
+                        ),
+                        circularStrokeCap: CircularStrokeCap.round,
+                        progressColor: _getScoreColor(recentQuiz['quizScore']),
+                        backgroundColor: _getScoreColor(
+                          recentQuiz['quizScore'],
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    difficulty.description,
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: AppTheme.text2,
-                    ),
-                    textAlign: TextAlign.center,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-        )
-        .animate(delay: Duration(milliseconds: 100 * index))
-        .slideY(begin: 0.5, end: 0, duration: Duration(milliseconds: 300))
-        .fadeIn();
+            )
+            .animate(delay: Duration(milliseconds: 100 * index))
+            .slideY(begin: 0.5, end: 0, duration: Duration(milliseconds: 300))
+            .fadeIn();
+      },
+    );
   }
 }
